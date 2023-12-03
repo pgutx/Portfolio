1dd3288f246ffafacb383c24bfe75fd671ba62f7
Piotr Gutowski <piotr.gutowski099@gmail.com>
2023-12-03 21:15:34 +0100

    Second commit - Imported fonts - Imported svg icons to use on the website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Titillium Web", ...defaultTheme.fontFamily.sans],
+			  },
+		},
 	},
 	plugins: [],
 }
